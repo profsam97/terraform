8fd1c15a138b35bbfe62227c78b58171c0e1fd26
profsam97 <proftoby4real@hotmail.com>
2024-04-14 16:56:02 +0100

    modified bash script

--- exercise3/web.sh
+++ exercise3/web.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 yum install https wget unzip -y
-systemctl start https
-systemctl enable https
+systemctl start httpd
+systemctl enable httpd
 wget https://www.tooplate.com/zip-templates/2133_moso_interior.zip
 unzip -o 2133_moso_interior.zip
-cp -r 2133_moso_interior/* /var/www/html
-systemctl restart https
+cp -r 2133_moso_interior/* /var/www/html/
+systemctl restart httpd
