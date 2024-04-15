41ddd53595deeca5373c369618957db0c7b50afa
profsam97 <proftoby4real@hotmail.com>
2024-04-15 15:30:30 +0100

    added security group

--- exercise6/secgrp.tf
+++ exercise6/secgrp.tf
@@ -1,9 +1,28 @@
 resource "aws_security_group" "hope-stack-sg" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
+  name        = "hope stack sg"
+  description = "Security group for hope ssh"
   vpc_id      = aws_vpc.hope_vpc
 
+      egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress  {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.AMIS]
+  }
+  ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tpc"
+        cidr_blocks = [var.MYIP]
+  }
   tags = {
-    Name = "allow_tls"
+    Name = "hope-stack-group"
   }
 }
