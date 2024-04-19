fb39d73bff77183c3d4569fc4d0c49b17f116f11
profsam97 <proftoby4real@hotmail.com>
2024-04-19 16:40:24 +0100

    removed wget

--- exercise8/webapp.sh
+++ exercise8/webapp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt install wget git -y
+sudo apt install git -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 nvm install 20
 sudo git clone https://github.com/profsam97/eyefront.git
-sudo cd eyefront
+cd eyefront
 npm install
 npm run build 
