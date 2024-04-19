e76dd2d9032600213490b9d62436e8126147b93c
profsam97 <proftoby4real@hotmail.com>
2024-04-19 17:39:13 +0100

    added node install

--- exercise8/webapp.sh
+++ exercise8/webapp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt install git -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-nvm install 20
+sudo apt install git nodejs -y
 sudo git clone https://github.com/profsam97/eyefront.git
+node -v
+npm -v
 cd eyefront
 npm install
 npm run build 
