e30c0cd656e5a0a2b86122d57c91e71c4394b498
profsam97 <proftoby4real@hotmail.com>
2024-04-19 18:14:36 +0100

    changeed git repo

--- exercise8/webapp.sh
+++ exercise8/webapp.sh
@@ -2,7 +2,7 @@
 sudo apt install git -y
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo git clone https://github.com/profsam97/eyefront.git
+sudo git clone https://github.com/profsam97/Ryada.git
 node -v
 npm -v
 cd eyefront
