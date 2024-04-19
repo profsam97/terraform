2271a09fca075392eee09dcf775db7eb506ebed1
profsam97 <proftoby4real@hotmail.com>
2024-04-19 17:50:31 +0100

    added node install

--- exercise8/webapp.sh
+++ exercise8/webapp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo apt install git nodejs -y
+sudo apt install git -y
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
 sudo git clone https://github.com/profsam97/eyefront.git
 node -v
 npm -v
